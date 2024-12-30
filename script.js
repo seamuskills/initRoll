fbf6f95414a3edb94b6839db1ddc72b0a149a2ad
seamuskills <thedarkknite10@gmail.com>
2024-12-29 18:04:19 -0600

    Character Entry is finished

--- script.js
+++ script.js
@@ -18,12 +18,46 @@ miscModInput.type = "number"
 miscModInput.id = "miscMod"
 charTemplate.appendChild(miscModInput)
 
-chars = [] //entry format: {name:"text", dex: 0+, mod:0+}
+deleteButton = document.createElement("button")
+deleteButton.textContent = "X"
+deleteButton.id = "trash"
+charTemplate.appendChild(deleteButton)
+
+chars = [] //entry format: {name:"text", dex: 0, mod:0}
+
+function removeChar(index){
+    chars.splice(index, 1)
+    charDiv.childNodes[index].remove()
+}
+
+function modifyProperty(index, input, property){
+    chars[index][property] = input
+}
 
 function addChar(){
+    chars.push({name:"undefined", dex:0, mod:0})
+    let clone = charTemplate.cloneNode(true)
+    clone.childNodes[0].addEventListener("input",(event) => {
+        let index = Array.from(charDiv.children).indexOf(clone);
+        modifyProperty(index, event.target.value, "name")
+    })
+    clone.childNodes[1].addEventListener("input",(event) => {
+        let index = Array.from(charDiv.children).indexOf(clone);
+        modifyProperty(index, Number(event.target.value), "dex")
+    })
+    clone.childNodes[2].addEventListener("input",(event) => {
+        let index = Array.from(charDiv.children).indexOf(clone);
+        modifyProperty(index, Number(event.target.value), "mod")
+    })
+    clone.childNodes[3].addEventListener("click", (event) => {
+        let index = Array.from(charDiv.children).indexOf(clone);
+        removeChar(index)
+    })
+    charDiv.insertBefore(clone, plusButton)
 }
 
 plusButton = document.createElement("button")
 plusButton.textContent = "+"
 plusButton.id = "addButton"
+plusButton.addEventListener("click", addChar)
 charDiv.appendChild(plusButton)
